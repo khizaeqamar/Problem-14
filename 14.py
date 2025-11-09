692f372bec1af09a707d58729ad445e19898cb3e
Khizar Qamar <khizarqamar345@gmail.com>
2025-11-09 16:29:31 +0500

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,7 @@
+#Average of Numbers
+a = float(input("Enter 1st number: "))
+b = float(input("Enter 2nd number: "))
+c = float(input("Enter 3rd number: "))
+
+avg = (a + b + c) / 3
+print("Average is:", avg)
